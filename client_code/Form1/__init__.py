06b6c601f61abd069f2f4a5e81dde15721961255
sowmyas1117@gmail.com <sowmyas1117@gmail.com>
2023-10-12 10:25:31 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -15,3 +15,30 @@ class Form1(Form1Template):
 
 
     # Any code you write here will run before the form opens.
+
+
+  def text_box_1_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    
+
+  def text_box_2_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    
+
+  def text_area_1_change(self, **event_args):
+    """This method is called when the text in this text area is edited"""
+    
+
+  def submit_Button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    name = self.text_box_1.text
+    email = self.text_box_2.text
+    FeedbackForm = self.text_area_1.text
+    alert('Feedback submited')
+
+
+
+
+
+
+
